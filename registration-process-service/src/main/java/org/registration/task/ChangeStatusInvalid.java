1868453fc996acf378ac19cd773d328ac452b26c
Ivan_Agafonov1 <sudden1306@gmail.com>
2021-06-23 00:53:41 +0300

    add field inject test

--- registration-process-service/src/main/java/org/registration/task/ChangeStatusInvalid.java
+++ registration-process-service/src/main/java/org/registration/task/ChangeStatusInvalid.java
@@ -1,6 +1,8 @@
 package org.registration.task;
 
+import net.bytebuddy.implementation.FixedValue;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.Expression;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -8,8 +10,11 @@ import org.springframework.stereotype.Component;
 @Component
 @Qualifier("changeStatusInvalid")
 public class ChangeStatusInvalid implements JavaDelegate {
+    private Expression status;
+
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
         delegateExecution.setVariable("status", "INVALID");
+        System.out.println("status: " + status.getValue(delegateExecution).toString() + "  " + delegateExecution.getVariable("status2"));
     }
 }
